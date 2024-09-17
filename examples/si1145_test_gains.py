dbf0d97aa941482c841bd297f1ee92b103fc57cb
Aaron W Morris <aaron@aarmor.net>
2024-09-17 10:10:00 -0500

    update copyright notices

--- examples/si1145_test_gains.py
+++ examples/si1145_test_gains.py
@@ -1,5 +1,4 @@
-# SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
-# SPDX-FileCopyrightText: Copyright (c) 2022 Carter Nelson for Adafruit Industries
+# SPDX-FileCopyrightText: Copyright (c) 2024 Aaron W Morris (aaronwmorris)
 #
 # SPDX-License-Identifier: Unlicense
 
